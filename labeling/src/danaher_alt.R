535b25a30efb10c9c8d53632efc202f650bf94e3
sfpacman <mrericyu@gmail.com>
2017-08-11 20:59:58 +0100

    Update danaher_alt.R

--- labeling/src/danaher_alt.R
+++ labeling/src/danaher_alt.R
@@ -2,7 +2,7 @@
 
 for(i in 1:length(cell_list)){
   type= cell_list[i]
-  type_gene_select <- match(unlist(cell_list[i]),nor_exp$gene$use_genes_n_id[1:1000])
+  type_gene_select <- match(unlist(cell_list[i]),nor_exp$gene$use_genes_n_id)
   type_gene_select <- type_gene_select[!is.na(type_gene_select)]
   type_expr <- nor_exp$exp[,type_gene_select]
   type_mean <- colMeans(t(type_expr))
@@ -10,9 +10,9 @@ for(i in 1:length(cell_list)){
 }
 
 colnames(mean_score) <- names(cell_list)
-cluster_mean <- cbind(Cluster=nor_exp$k$cluster,mean_score)
+cluster_mean <- data.frame(cbind(Cluster=nor_exp$k$cluster,mean_score))
 score_by_cluster <- round(cluster_mean %>% group_by(Cluster) %>% summarise_all(funs(mean)),3)
-cluster_type <-apply(score_by_cluster, 1,function(x) which(x == max(x)))
+cluster_type <-apply(score_by_cluster[,-which(names(cluster_mean) %in% c("Cluster"))], 1,function(x) which(x == max(x)))
 cluster_type <-data.frame(Cluster=1:length(cluster_type),cell_type=cluster_type, name_type=names(cell_list)[cluster_type])
 cluster_assignment <-merge( x=nor_exp$k , y=cluster_type, by.x="k", by.y="Cluster")
 all_gene_var_pos <- list()
